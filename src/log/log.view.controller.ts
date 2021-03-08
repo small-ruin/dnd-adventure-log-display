55db5a10c14a65ac5155289becf50b189db22722
hilshire <hillshire.triela@gmail.com>
2021-03-08 23:02:37 +0800

    for product

--- src/log/log.view.controller.ts
+++ src/log/log.view.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get, Param, Render } from "@nestjs/common";
-import { LogService } from "./log.service";
+import { Controller, Get, Param, Render, Req, Res } from "@nestjs/common";
+import { Response } from "express";
+import { LogService  } from "./log.service";
+import { join } from 'path';
 
 @Controller('log')
 export class LogViewController {
     constructor(private service: LogService) {}
     
+    @Get('/')
+    get(@Res() res: Response) {
+        // change the path to the correct html page path in your project
+        res.sendFile(join(process.cwd(), 'public/index.html'));
+    }
+
     @Get(':id')
     @Render('log')
     async findById(@Param('id') id) {
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: '/static' });
+  app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: '/' });
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
